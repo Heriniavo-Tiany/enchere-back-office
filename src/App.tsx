84df9c9171a028c1782d8cd8c430990d189a3d3a
Heriniavo <sampilahy.heriniavo.com>
2023-01-13 17:45:41 +0300

    Inscription

--- src/App.tsx
+++ src/App.tsx
@@ -22,6 +22,7 @@ import '@ionic/react/css/display.css';
 
 /* Theme variables */
 import './theme/variables.css';
+import Inscription from "./pages/Inscription";
 
 setupIonicReact();
 
@@ -38,6 +39,11 @@ const App: React.FC = () => {
             <Route path="/page/:name" exact={true}>
               <Page />
             </Route>
+
+            <Route path="/inscription" exact={true}>
+              <Inscription />
+            </Route>
+
           </IonRouterOutlet>
         </IonSplitPane>
       </IonReactRouter>
